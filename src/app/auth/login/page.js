e812200cb6b173121bd53edc876f97559efe48ff
StefanoDeSa <stefanobritodesa@gmail.com>
2024-08-30 21:23:47 -0300

    fix: corrige caminho do text helper do login para o register

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -62,7 +62,7 @@ export default function Login() {
                         required
                     />
                 </div>
-                <p id="helper-text-explanation" className="mt-2 mb-7 text-sm text-gray-500 dark:text-gray-400">Caso ainda não tenha uma conta, faça o seu <Link href="/register" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Registro</Link>.</p>
+                <p id="helper-text-explanation" className="mt-2 mb-7 text-sm text-gray-500 dark:text-gray-400">Caso ainda não tenha uma conta, faça o seu <Link href="register" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Registro</Link>.</p>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
             </form>
         </div>
