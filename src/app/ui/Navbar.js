3d61b74e64f8edcf0105ec1e232db58e3372b300
StefanoDeSa <stefanobritodesa@gmail.com>
2024-08-31 10:36:52 -0300

    feat: adiciona validação na navbar

--- src/app/ui/Navbar.js
+++ src/app/ui/Navbar.js
@@ -1,8 +1,29 @@
-
+'use client'
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { getCookie, setCookie } from "../lib/utils/cookie";
+import { useRouter } from "next/navigation";
 
 export function NavbarLayout() {
+  const [userEmail, setUserEmail] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Busca o cookie do userEmail
+    const email = getCookie('userEmail');
+    if (email) {
+      setUserEmail(email);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    // Remove o cookie do userEmail
+    document.cookie = `userEmail=${''}; path=/;`;
+    setUserEmail(null);
+    router.push('auth/login'); // Redireciona para a página de login
+  };
+
   return (
     <Navbar fluid rounded>
       <NavbarBrand as={Link} href="/">
@@ -10,12 +31,23 @@ export function NavbarLayout() {
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink as={Link} href="/profile">
-          Perfil
-        </NavbarLink>
-        <NavbarLink as={Link} href="/docs">
-          Documentos
-        </NavbarLink>
+        {userEmail ? (
+          <>
+            <NavbarLink as={Link} href="/profile">
+              Perfil
+            </NavbarLink>
+            <NavbarLink as={Link} href="/docs">
+              Documentos
+            </NavbarLink>
+            <NavbarLink as="button" onClick={handleLogout}>
+              Sair
+            </NavbarLink>
+          </>
+        ) : (
+          <NavbarLink as={Link} href="auth/login">
+            Entrar
+          </NavbarLink>
+        )}
         <NavbarLink as={Link} href="/">
           Validar
         </NavbarLink>
