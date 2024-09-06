7d2aa9148d17760a60930e8a7af7dfceeffb7422
StefanoDeSa <stefanobritodesa@gmail.com>
2024-09-06 13:54:20 -0300

    fix: corrige posicionamento da capa

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
-      <div className="container grid items-center justify-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
-        <div className="space-y-4">
+    <section className="w-full py-12 md:py-24 lg:py-32 flex items-center justify-center">
+      <div className="container flex flex-col lg:flex-row items-center justify-center gap-6 px-4 md:px-6 lg:gap-10">
+        <div className="space-y-4 text-center lg:text-left">
           <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
             Assine documentos com segurança e confiança
           </h2>
@@ -21,16 +21,18 @@ export default function Home() {
               </span>
             </button>
           </Link>
-
         </div>
-        <Image
-          src="/digital-signature-cover.webp"
-          width="550"
-          height="310"
-          alt="Assinatura Digital"
-          className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
-        />
+        <div className="flex justify-center">
+          <Image
+            src="/digital-signature-cover.webp"
+            width="550"
+            height="310"
+            alt="Assinatura Digital"
+            className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center"
+          />
+        </div>
       </div>
     </section>
+
   )
 }
